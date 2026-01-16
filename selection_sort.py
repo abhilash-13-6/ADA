f7769c6209ddea37ba6b251fbe79a157e53aec25
Abhilash S Devadiga <abhilashsdevadiga@gmail.com>
2026-01-16 10:37:24 +0530

    Create selection_sort.py

---
+++ selection_sort.py
@@ -0,0 +1,18 @@
+ef selection_sort(arr):
+    n = len(arr)
+
+    for i in range(n - 1):
+        min_index = i
+
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+
+    return arr
+
+
+data = [64, 12, 25, 24, 48]
+sorted_data = selection_sort(data)
+print("sorted array:", sorted_data)
