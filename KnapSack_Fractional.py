b417564b00cc9d95066ab9ad9a7e415b0b42e831
Abhilash S Devadiga <abhilashsdevadiga@gmail.com>
2026-02-27 10:31:17 +0530

    Create KnapSack_Fractional.py

---
+++ KnapSack_Fractional.py
@@ -0,0 +1,13 @@
+# Time Complexity: O(n log n)
+
+items = []
+n = int(input("Enter number of items: "))
+
+for i in range(n):
+    value = int(input("Enter value: "))
+    weight = int(input("Enter weight: "))
+    items.append((value,weight))
+
+capacity = int()
+
+# TODO: incomplete
